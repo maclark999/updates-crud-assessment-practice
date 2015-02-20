c482ad08d28d0ad628ebe1a62c0b9df15eaa25c8
mackenzie clarkson <maclark99@gmail.com>
2015-02-20 11:33:01 -0700

    user can add new status update

--- practice_crud/app/controllers/updates_controller.rb
+++ practice_crud/app/controllers/updates_controller.rb
@@ -1,15 +1,24 @@
 class UpdatesController < ApplicationController
+before_action :set_update, only: [:show, :edit, :update, :destroy]
   def index
     @updates = Update.all
   end
 
   def new
+    @update = Update.new
   end
 
   def create
+    @update = Update.new(update_params)
+    if @update.save
+      redirect_to @update
+    else
+      render :new
+    end
   end
 
   def show
+    
   end
 
   def edit
@@ -20,5 +29,14 @@ class UpdatesController < ApplicationController
 
   def destroy
   end
+
+  private
+    def update_params
+      params.require(:update).permit(:status, :user, :number_of_likes)
+    end
+
+    def set_update
+      @update = Update.find(params[:id])
+    end
 
 end
